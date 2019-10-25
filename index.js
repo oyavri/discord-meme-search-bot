9f9df2598a1221a7037769a3407c5e78cc8c3250
Berkan Yavri <berkan.yavri@deliveryhero.com>
2019-10-25 19:47:41 +0200

    Add starter command

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js');
-const client = Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
-
+  console.log('Bot ready');
 });
 
-client.on('message', () => {
+client.on('message', (msg) => {
+  const {channel, content, author} = msg;
 
+  if (author.bot) {
+    return null;
+  }
+
+  const message = `Greetings ${channel.name}! Unfortunately I am unable to respond to "${content}" at the moment.`;
+
+  msg.reply(message)
+    .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
+    .catch(console.error);
 });
 
-client.login('some-token');
+client.login(process.env.DISCORD_TOKEN || 'some-token');
